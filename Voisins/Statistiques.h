08478943350389219f1fa673f035d1969c57847e
Victo-2343383 <2343383@carrefour.cegepvicto.ca>
2025-01-23 10:47:43 -0500

    déclaration des statistiques

---
+++ Voisins/Statistiques.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+namespace Statistiques
+{
+	float Moyenne(std::vector<float> liste);
+	float Medianne(std::vector<float> liste);
+	float EcartType(std::vector<float> liste);
+	float DistEuclidienne(std::vector<float> liste);
+	float DistManhattan(std::vector<float> liste);
+};
+
